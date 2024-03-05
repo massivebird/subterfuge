8e46fb35c3e23626d623bd6bbe93496707fcea70
massivebird <gdrakemail@gmail.com>
2024-03-05 00:23:19 -0500

    feat: successful api call w steam id

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use std::fs::read_to_string;
 
+fn main() {
+    let api_key = &read_to_string("/home/penguino/sandbox/steam_api_key").unwrap();
+    let api_key = api_key.trim();
+
+    // mine: 76561198748465236
+    let steam_id = "76561198748465236";
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.get("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/")
+        .query(&[
+            ("key", api_key),
+            ("steamid", steam_id),
+            ("format", "json"),
+        ])
+        .send()
+        .unwrap();
+
+    dbg!(response.text());
 }
