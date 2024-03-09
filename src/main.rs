ce2aa7739420af73eadc6528f75c4db35bd40e23
massivebird <gdrakemail@gmail.com>
2024-03-09 01:34:45 -0500

    feat: a debugging message

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,6 @@ fn main() {
 
     for id in steam_ids {
         std::thread::spawn(move || analyze_user(&id.unwrap()));
-        // stagger threads
-        thread::sleep(Duration::new(3 /* secs */, 0 /* nanos */));
     }
 
     // let those threads do their thing :3
@@ -31,76 +29,87 @@ fn main() {
 fn analyze_user(steam_id: &str) {
     let api_key = &read_to_string("/home/penguino/sandbox/steam_api_key").unwrap();
     let api_key = api_key.trim();
+
+    let request = reqwest::blocking::Client::new()
+        .get("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/")
+        .query(&[("key", api_key), ("steamid", steam_id), ("format", "json")]);
+
+    let user = User::new(api_key, steam_id);
+    let persona_name = &user.persona_name;
+
+    log(&format!("Initialized user {user}"));
+
+    // Used to calculate game session length
+    let mut games_cache: Vec<Game> = Vec::new();
+
     loop {
-        let user = User::new(api_key, steam_id);
-        log(&format!("Initialized user {user}"));
-        let persona_name = &user.persona_name;
+        thread::sleep(Duration::new(90 /* secs */, 0 /* nanos */));
 
-        let request = reqwest::blocking::Client::new()
-            .get("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/")
-            .query(&[("key", api_key), ("steamid", steam_id), ("format", "json")]);
+        let Ok(response) = request.try_clone().unwrap().send() else {
+            log(&format!("WARNING: request for {user} failed."));
+            continue;
+        };
 
-        let mut games_cache: Vec<Game> = Vec::new();
+        let response_text: String = response.text().unwrap();
 
-        loop {
-            thread::sleep(Duration::new(90 /* secs */, 0 /* nanos */));
+        let Ok(parsed) = json::parse(&response_text) else {
+            // JSON parsing fails sometimes because HTML is returned instead.
+            // Could be a request timeout. Let's find out!
+            dbg!(response_text);
+            log(&format!(
+                "JSON parsing failed for {persona_name}. See above for details."
+            ));
+            continue;
+        };
 
-            let Ok(response) = request.try_clone().unwrap().send() else {
-                log(&format!("WARNING: request for {user} failed."));
-                continue;
-            };
+        let games: Vec<Game> = parsed["response"]["games"]
+            .members()
+            .map(|g| {
+                Game::new(
+                    g["name"].to_string(),
+                    g["appid"].as_u32().unwrap(),
+                    g["playtime_forever"].as_u32().unwrap(),
+                )
+            })
+            .collect();
 
-            let games: Vec<Game> = json::parse(&response.text().unwrap()).unwrap()["response"]
-                ["games"]
-                .members()
-                .map(|g| {
-                    Game::new(
-                        g["name"].to_string(),
-                        g["appid"].as_u32().unwrap(),
-                        g["playtime_forever"].as_u32().unwrap(),
-                    )
-                })
-                .collect();
+        if games_cache.is_empty() {
+            games_cache = games;
+            continue;
+        }
 
-            if games_cache.is_empty() {
-                games_cache = games;
-                continue;
-            }
+        // if games are unchanged since last cache, nothing to report
+        if games.iter().all(|g| games_cache.iter().any(|o| o == g)) {
+            continue;
+        }
 
-            // if games are unchanged since last cache, nothing to report
-            if games.iter().all(|g| games_cache.iter().any(|o| o == g)) {
-                continue;
-            }
+        // find the discrepant game that corresponds to none in the cache
+        let latest_game: &Game = games
+            .iter()
+            .find(|&g| !games_cache.iter().any(|o| o == g))
+            .unwrap();
+        let game_name = &latest_game.name;
+        let total_playtime = latest_game.playtime_forever;
 
-            // find the discrepant game that corresponds to none in the cache
-            let latest_game: &Game = games
-                .iter()
-                .find(|&g| !games_cache.iter().any(|o| o == g))
-                .unwrap();
-            let game_name = &latest_game.name;
-            let total_playtime = latest_game.playtime_forever;
-
-            // this game was cached only if it has been played in the last two weeks;
-            // otherwise, we have no previous playtime to compare to.
-            let Some(latest_game_cached) =
-                games_cache.iter().find(|g| g.app_id == latest_game.app_id)
-            else {
-                log(
+        // this game was cached only if it has been played in the last two weeks;
+        // otherwise, we have no previous playtime to compare to.
+        let Some(latest_game_cached) = games_cache.iter().find(|g| g.app_id == latest_game.app_id)
+        else {
+            log(
                     &format!("{persona_name} has been playing {game_name} for the first time in the last two weeks. Total of {total_playtime} minute(s).")
                );
-                games_cache = games;
-                continue;
-            };
+            games_cache = games;
+            continue;
+        };
 
-            let prev_playtime = latest_game_cached.playtime_forever;
-            let delta_total_playtime = total_playtime - prev_playtime;
+        let prev_playtime = latest_game_cached.playtime_forever;
+        let delta_total_playtime = total_playtime - prev_playtime;
 
-            log(
+        log(
                 &format!("{persona_name} has been playing {game_name}. Played for {delta_total_playtime} minute(s). Total of {total_playtime} minute(s).")
             );
 
-            games_cache = games;
-        }
+        games_cache = games;
     }
 }
 
