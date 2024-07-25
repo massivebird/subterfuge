4d48979feaca2bd4797e01190f8c79b0cb8f688a
massivebird <gdrakemail@gmail.com>
2024-07-25 03:13:43 -0400

    added: user IDs argument

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,13 @@ fn main() {
                 .value_hint(clap::ValueHint::FilePath)
                 .value_name("PATH")
                 .help("Path to the YAML config file."),
+            Arg::new("user_ids")
+                .long("user-ids")
+                .alias("users")
+                .conflicts_with("config")
+                .required(false)
+                .value_name("user-IDs")
+                .help("Comma-separated user IDs."),
         ])
         .get_matches();
 
